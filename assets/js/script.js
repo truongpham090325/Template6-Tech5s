f1035f6e395796e55bfcfd605a050acc994ca0ae
Phạm Quang Trường <truongpham090325@gmail.com>
2026-04-07 10:51:36 +0700

    Xong section-8

--- assets/js/script.js
+++ assets/js/script.js
@@ -113,3 +113,52 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 // End Accordion Section-7
+
+// Clock Expire
+const clockExpire = document.querySelector("[clock-expire]");
+if (clockExpire) {
+  const expireDateTimeString = clockExpire.getAttribute("clock-expire");
+
+  // Chuyển đổi chuỗi thời gian thành đối tượng Date
+  const expireDateTime = new Date(expireDateTimeString);
+
+  // Hàm cập nhật đồng hồ
+  const updateClock = () => {
+    const now = new Date();
+    const remainingTime = expireDateTime - now; // quy về đơn vị mili giây
+
+    if (remainingTime > 0) {
+      const days = Math.floor(remainingTime / (24 * 60 * 60 * 1000));
+      // Tính số ngày, 24 * 60 * 60 * 1000 Tích của các số này = số mili giây trong 1 ngày
+
+      const hours = Math.floor((remainingTime / (60 * 60 * 1000)) % 24);
+      // Tính số giờ, 60 * 60 * 1000 Chia remainingTime cho giá trị này để nhận được tổng số giờ.
+      // % 24 Lấy phần dư khi chia tổng số giờ cho 24 để chỉ lấy số giờ còn lại trong ngày.
+
+      const minutes = Math.floor((remainingTime / (60 * 1000)) % 60);
+      // Tính số phút, 60 * 1000 Chia remainingTime cho giá trị này để nhận được tổng số phút.
+      // % 60 Lấy phần dư khi chia tổng số phút cho 60 để chỉ lấy số phút còn lại trong giờ.
+
+      const seconds = Math.floor((remainingTime / 1000) % 60);
+      // Tính số giây, 1000 Chia remainingTime cho giá trị này để nhận được tổng số giây.
+      // % 60 Lấy phần dư khi chia tổng số giây cho 60 để chỉ lấy số giây còn lại trong phút.
+
+      // Cập nhật giá trị vào thẻ span
+      const listBoxNumber = clockExpire.querySelectorAll(".inner-number");
+      listBoxNumber[0].innerHTML = `${days}`.padStart(2, "0");
+      listBoxNumber[1].innerHTML = `${hours}`.padStart(2, "0");
+      listBoxNumber[2].innerHTML = `${minutes}`.padStart(2, "0");
+      listBoxNumber[3].innerHTML = `${seconds}`.padStart(2, "0");
+    } else {
+      // Khi hết thời gian, dừng đồng hồ
+      clearInterval(intervalClock);
+    }
+  };
+
+  // Gọi ngay lần đầu tiên để tránh bị trễ 1 giây
+  updateClock();
+  
+  // Gọi hàm cập nhật đồng hồ mỗi giây
+  const intervalClock = setInterval(updateClock, 1000);
+}
+// End Clock Expire
